5b83c353ff95be345ccc3d6d2782b369a950c82e
Kana Ohtake <kaoh2020@tisbrain.net>
2020-07-25 14:21:14 +0900

    v

---
+++ components/Header.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="full">
+   <h1> 高校生の高校生による高校生のための黒人差別問題</h1>
+
+    <div class="toggle">
+      <nav class="menu">
+        <a class="bar">トップ</a>
+        <a class="bar">歴史</a>
+        <a class="bar">現問題</a>
+        <a class="bar">社会影響</a>
+        <a class="bar">スポーツ</a>
+        <a class="bar">組織</a>
+        <a class="bar">他国での差別</a>
+        <a class="bar">他の差別</a>
+       </nav>
+     </div>
+  </div> 
+           
+  </div>
+</template>
+
+<script>
+export default {
+    name: "Header",
+}
+
+</script>
+
+<style>
+
+h1{
+    font-size: 50px; 
+}
+
+</style>
