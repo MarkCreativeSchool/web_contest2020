2b344499fc6ec74e48719b8e0b14b0b3432879e8
Kana Ohtake <kaoh2020@tisbrain.net>
2020-07-25 15:48:34 +0900

    navbar

--- components/Header.vue
+++ components/Header.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="full">
+   <div class="up">
    <h1> 高校生の高校生による高校生のための黒人差別問題</h1>
-
+    </div>
     <div class="toggle">
-      <nav class="menu">
         <a class="bar">トップ</a>
         <a class="bar">歴史</a>
         <a class="bar">現問題</a>
@@ -12,8 +12,8 @@
         <a class="bar">組織</a>
         <a class="bar">他国での差別</a>
         <a class="bar">他の差別</a>
-       </nav>
-     </div>
+        <a class="bar">その他</a>
+        <a class="bar">ENGLISH VERSION</a>
   </div> 
            
   </div>
@@ -28,8 +28,36 @@ export default {
 
 <style>
 
+.up{
+    padding-top: 10px; 
+    text-align: center; 
+    background-color: #000000;
+    color: #ffffff; 
+}
 h1{
     font-size: 50px; 
+    font-family: "Rockwell"; 
 }
+
+.bar{
+  float: left;
+  color: #ffffff;
+  text-align: center;
+  padding: 17px 40px;
+  text-decoration: none;
+  font-size: 17px;
+}
+.menu{
+    width: 100%; 
+}
+
+.toggle{
+background-color: #333;
+  overflow: hidden;
+}
+
+
+
+
 
 </style>
