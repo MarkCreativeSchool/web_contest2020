75c92a83094007a44bd5bf30dfb735e23cd044b4
KENKUN-1031 <kencoding1031@gmail.com>
2020-10-10 16:21:02 +0900

    timeline2

--- pages/history.vue
+++ pages/history.vue
@@ -11,17 +11,41 @@
         <div class="line">
           <div class="linebar line2"></div>
         </div>
-        <div class="line">3</div>
-        <div class="line">4</div>
-        <div class="line">5</div>
-        <div class="line">6</div>
-        <div class="line">7</div>
-        <div class="line">8</div>
-        <div class="line">9</div>
-        <div class="line">10</div>
+        <div class="line">
+          <div class="linebar line3"></div>
+        </div>
+        <div class="line">
+          <div class="linebar line4"></div>
+        </div>
+        <div class="line">
+          <div class="linebar line5"></div>
+        </div>
+        <div class="line">
+          <div class="linebar line6"></div>
+        </div>
+        <div class="line">
+          <div class="linebar line7"></div>
+        </div>
+        <div class="line">
+          <div class="linebar line8"></div>
+        </div>
+        <div class="line">
+          <div class="linebar line9"></div>
+        </div>
+        <div class="line">
+          <div class="linebar line10"></div>
+        </div>
+        <div class="year">
+          <h6>1400</h6>
+          <h6>1500</h6>
+          <h6>1600</h6>
+          <h6>1700</h6>
+          <h6>1800</h6>
+          <h6>1900</h6>
+          <h6>2000</h6>
+        </div>
       </div>
    </div>
-    
      <Footer></Footer>
   </div>
 </template>
@@ -83,14 +107,59 @@ export default {
   height: 20px;
 }
 .timeline .line .linebar{
-  width: 200px;
-  height: 2px;
-  top: 5px;
-  background: blue;
+  height: 5px;
+  top: 3px;
+  background: black;
 }
 .line1{
   position: absolute;
   left: 20px;
 }
-
+.line2{
+  position: absolute;
+  left: 60px;
+}
+.line3{
+  position: absolute;
+  left: 100px;
+}
+.line4{
+  position: absolute;
+  left: 140px
+}
+.line5{
+  position: absolute;
+  left: 160px
+}
+.line6{
+  position: absolute;
+  left: 180px
+}
+.line7{
+  position: absolute;
+  left: 200px;
+}
+.line8{
+  position: absolute;
+  left: 220px;
+}
+.line9{
+  position: absolute;
+  left: 250px;
+}
+.line10{
+  position: absolute;
+  left: 20px;
+  width: calc(100% - 40px);
+}
+.year{
+  display: flex;
+  align-content: space-between;
+  width: 100%;
+  text-align: center;
+}
+.year h6{
+  flex: 1;
+  font-size: 17px;
+}
 </style>
