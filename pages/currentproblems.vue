14823139656a66c763bf6ca41b559d1386dd5df5
Kensei Yoshida <saryusoul@Saryus-MacBook-Pro.local>
2020-10-10 12:04:08 +0900

    currentproblems

--- pages/currentproblems.vue
+++ pages/currentproblems.vue
@@ -1,17 +1,35 @@
 <template>
-  <div class="full">
-  <Header></Header>
-   <div class="info">
-    
-    <h3 class="start">現問題・抗議活動・出来事</h3>
-    <p class="two">下にある地図は２００６年から現在まで米国で起こった様々の黒人差別問題を紹介しています。
+  <div>
+    <div class="parent">
+      <div class="box or" @click="onClick('or')">or</div>
+      <div class="box ca" @click="onClick('ca')">ca</div>
+      <div class="box mn" @click="onClick('mn')">mn</div>
+      <div class="box mo" @click="onClick('mo')">mo</div>
+      <div class="box il" @click="onClick('il')">il</div>
+      <div class="box ky" @click="onClick('ky')">ky</div>
+      <div class="box oh" @click="onClick('oh')">oh</div>
+      <div class="box ny" @click="onClick('ny')">ny</div>
+      <div class="box ma" @click="onClick('ma')">ma</div>
+      <div class="box pa" @click="onClick('pa')">pa</div>
+      <div class="box md" @click="onClick('md')">md</div>
+      <div class="box ga" @click="onClick('ga')">ga</div>
+      <div class="box fl" @click="onClick('fl')">fl</div>
+      <div class="space"></div>
+    </div>
+    <div class="full">
+    <Header></Header>
+     <div class="info">
 
-    </p>
+      <h3 class="start">現問題・抗議活動・出来事</h3>
+      <p class="two">下にある地図は２００６年から現在まで米国で起こった様々の黒人差別問題を紹介しています。
 
-<img class="map" src="~/assets/usmap.png">
-    </div> 
-    
-     <Footer></Footer>
+      </p>
+
+      <img class="map" src="~/assets/usmap.png">
+      </div>
+
+       <Footer></Footer>
+    </div>
   </div>
 </template>
 
@@ -27,47 +45,139 @@ export default {
 <style>
 
 @import url('https://fonts.googleapis.com/css?family=Noto+Sans+JP');
-.two{
-  font-family: "Noto Sans JP"; 
-  width: 83%; 
-  margin: 0 auto;
-  padding-top: 20px; 
-  font-size: 18px; 
-}
+  .two{
+    font-family: "Noto Sans JP";
+    width: 83%;
+    margin: 0 auto;
+    padding-top: 20px;
+    font-size: 18px;
+  }
 
 
-.start{
-  font-family: "Noto Sans JP"; 
-  color: #ffffff;
-  background-color: #000000;
-  background-position: center center;
-  width: 83%; 
-  margin: 0 auto;
-  font-size: 30px; 
-  border-radius: 3px; 
-  text-shadow: 0.5px 0.5px #f6d405;
-  text-align: center; 
-}
+  .start{
+    font-family: "Noto Sans JP";
+    color: #ffffff;
+    background-color: #000000;
+    background-position: center center;
+    width: 83%;
+    margin: 0 auto;
+    font-size: 30px;
+    border-radius: 3px;
+    text-shadow: 0.5px 0.5px #f6d405;
+    text-align: center;
+  }
 
-.more{
-  font-family: "Noto Sans JP"; 
-  color: #ffffff;
-  background-color: #000000;
-  background-position: center center;
-  width: 73%; 
-  margin: 0 auto;
-  font-size: 30px; 
-  border-radius: 3px; 
-  text-shadow: 0.5px 0.5px #f6d405;
-  text-align: center; 
-  margin-top: 45px; 
-}
+  .more{
+    font-family: "Noto Sans JP";
+    color: #ffffff;
+    background-color: #000000;
+    background-position: center center;
+    width: 73%;
+    margin: 0 auto;
+    font-size: 30px;
+    border-radius: 3px;
+    text-shadow: 0.5px 0.5px #f6d405;
+    text-align: center;
+    margin-top: 45px;
+  }
 
-.map{
-  margin-left: auto;
-  margin-right: auto;
-  width: 80%; 
-  display: block;
-}
+  .map{
+    margin-left: auto;
+    margin-right: auto;
+    width: 1000px;
+    display: block;
+  }
+  .parent{
+    position: relative;
+    width: 1000px;
+    margin-left: auto;
+    margin-right: auto;
+  }
+  .box{
+    position:absolute;
+    background-color: #4CAF50;
+    opacity: 0.2;
+    cursor: pointer;
+    z-index: 10;
+  }
+  .or{
+    top: 450px;
+    left: 40px;
+    width: 115px;
+    height: 79px;
+  }
+  .ca{
+    top: 534px;
+    left: 49px;
+    width: 77px;
+    height: 215px;
+    transform: rotate(-25deg)
+  }
+  .mn{
+    top: 423px;
+    left: 530px;
+    width: 83px;
+    height: 107px;
+  }
+  .mo{
+    top: 629px;
+    left: 539px;
+    width: 91px;
+    height: 80px;
+  }
+  .il{
+    top: 563px;
+    left: 633px;
+    width: 50px;
+    height: 107px;
+  }
+  .ky{
+    top: 625px;
+    left: 686px;
+    width: 104px;
+    height: 55px;
+  }
+  .oh{
+    top: 531px;
+    left: 746px;
+    width: 63px;
+    height: 64px;
+  }
+  .ny{
+    top: 435px;
+    left: 810px;
+    width: 96px;
+    height: 73px;
+  }
+  .ma{
+    top: 432px;
+    left: 934px;
+    width: 44px;
+    height: 15px;
+  }
+  .pa{
+    top: 520px;
+    left: 817px;
+    width: 84px;
+    height: 53px;
+  }
+  .md{
+    top: 593px;
+    left: 863px;
+    width: 68px;
+    height: 55px;
+  }
+  .ga{
+    top: 748px;
+    left: 733px;
+    width: 81px;
+    height: 86px;
+  }
+  .fl{
+    top: 845px;
+    left: 720px;
+    width: 140px;
+    height: 110px;
+  }
 
 </style>
