9ad82a22ead4f51db00f9dd4f7c5a809b7867f4c
KENKUN-1031 <kencoding1031@gmail.com>
2020-11-06 13:22:28 +0900

    timeline

---
+++ components/events/2nd/event6.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="event">
+    <div class="layer" @click="onClose"></div>
+    <div class="border">
+      <h6 class="title"><b>終戦(1783年9月)</b></h6>
+      <hr>
+      <p class="context">1783年に締結したパリ条約によって、アメリカ独立戦争は終結した。この講和条約で、イギリスは13植民地を正式にアメリカ合衆国と認め、ミシシッピ以東のルイジアナをアメリカに割譲した。
+      </p>
+      <h6 class="xButton" @click="onClose">x</h6>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "event6.vue",
+  methods:{
+    onClose(){
+      this.$emit("onClose")
+    }
+  }
+}
+</script>
+
+<style scoped>
+.event .border{
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  height: 50%;
+  transform: translate(-50%, -50%);
+  width: 1000px;
+  background: #ffffff;
+  z-index: 2001;
+  border-radius: 15px;
+  border: solid 5px burlywood;
+}
+.event .border .xButton{
+  position: fixed;
+  right: 20px;
+  top: -5px;
+  font-size: 40px;
+  cursor: pointer;
+  font-family: "Comic Sans MS";
+  font-weight: bold;
+}
+.event .border .title{
+  position: relative;
+  top: 20px;
+  font-size: 50px;
+}
+.event .border .context{
+  position: fixed;
+  top: auto;
+  font-size: 45px;
+  font-weight: normal;
+  text-align: center;
+}
+.event .layer {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100vh;
+  z-index: 2000;
+  opacity: 0.2;
+}
+</style>
---
+++ components/timeline/line3.vue
@@ -0,0 +1,112 @@
+<template>
+  <div>
+    <div class="layer" @click="onClose"></div>
+    <div class="test">
+      <div class="title">第3章：独立戦争後のアメリカの歩み</div>
+      <!--イベント-->
+      <div class="event1" @click="onClick('event1')"></div>
+
+
+      <div class="linebar"></div>
+      <div class="year">
+        <h6>
+          <div class="timelineFirst">1800</div>
+        </h6>
+        <h6>
+          <div class="timelineLast">1815</div>
+        </h6>
+      </div>
+    </div>
+    <event1 v-if="locationName==='event1'" @onClose="locationName=null"></event1>
+  </div>
+</template>
+
+<script>
+import event1 from "~/components/events/1st/event1"
+
+export default {
+  name: "line1.vue",
+  components: {event1, },
+  data(){
+    return{
+      locationName:null
+    }
+  },
+  methods:{
+    onClose(){
+      this.$emit("onClose")
+    },
+    onClick(locationName2){
+      this.locationName=locationName2
+    }
+  }
+}
+</script>
+
+<style scoped>
+.test{
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  height: 50%;
+  transform: translate(-50%, -50%);
+  width: 1000px;
+  background: #ffffff;
+  z-index: 1001;
+  border-radius: 15px;
+  border: solid 5px burlywood;
+}
+.test .title{
+  position: relative;
+  left: 20px;
+}
+.test .linebar{
+  position: relative;
+  /*top: calc(85% - 110px);*/
+  bottom: calc(1% - 155px);
+  width: 90%;
+  margin: 0 auto;
+  height: 20px;
+  background: black;
+  border-radius: 7px;
+}
+.test .event1{
+  position:relative;
+  top: calc(10% - 35px);
+  left: 65px;
+  width: 20px;
+  height: 20px;
+  background: black;
+  cursor: pointer;
+  border-radius: 7px;
+}
+.year{
+  position: relative;
+  display: flex;
+  align-content: space-between;
+  width: 100%;
+  text-align: center;
+  /*top: calc(83% - 95px);*/
+  bottom: calc(1% - 160px);
+  flex: 1;
+  font-size: 17px;
+  font-weight: bold;
+}
+.year h6 .timelineFirst{
+  text-align: left;
+  padding-left: 50px;
+}
+.year h6 .timelineLast{
+  text-align: right;
+  padding-right: 50px;
+}
+.layer{
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0,0,0,0.7);
+  z-index: 1000;
+}
+</style>
