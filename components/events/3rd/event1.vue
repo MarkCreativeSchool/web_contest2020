b20ce19b91fda5ceb166fa06db41160bd3ca6f10
KENKUN-1031 <kencoding1031@gmail.com>
2020-11-06 13:37:18 +0900

    timeline3

---
+++ components/events/3rd/event1.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="event">
+    <div class="layer" @click="onClose"></div>
+    <div class="border">
+      <h6 class="title"><b>ルイジアナ買収(1803)</b></h6>
+      <hr>
+      <p class="context">アメリカ合衆国のジェファソン大統領が、フランスのナポレオンから、ミシシッピ側以西からロッキー山脈に及ぶ広大なルイジアナを買収。
+      </p>
+      <h6 class="xButton" @click="onClose">x</h6>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "event1.vue",
+  methods:{
+    onClose(){
+      this.$emit("onClose")
+    }
+  }
+}
+</script>
+
+<style scoped>
+.event .border{
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  height: 50%;
+  transform: translate(-50%, -50%);
+  width: 1000px;
+  background: #ffffff;
+  z-index: 2001;
+  border-radius: 15px;
+  border: solid 5px burlywood;
+}
+.event .border .xButton{
+  position: fixed;
+  right: 20px;
+  top: -5px;
+  font-size: 40px;
+  cursor: pointer;
+  font-family: "Comic Sans MS";
+  font-weight: bold;
+}
+.event .border .title{
+  position: relative;
+  top: 20px;
+  font-size: 50px;
+}
+.event .border .context{
+  position: fixed;
+  top: auto;
+  font-size: 45px;
+  font-weight: normal;
+  text-align: center;
+}
+.event .layer {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100vh;
+  z-index: 2000;
+  opacity: 0.2;
+}
+</style>
