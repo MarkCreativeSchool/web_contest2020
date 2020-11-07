cc616f897ff557020c505df2804c42c01e5e92c9
KENKUN-1031 <kencoding1031@gmail.com>
2020-11-07 15:55:47 +0900

    timeline17

--- components/events/1st/event6.vue
+++ components/events/1st/event6.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="event">
-    <div class="layer" @click="onClose"></div>
+  <div class="event-sub-modal">
+    <div class="sublayer" @click="onClose"></div>
     <div class="border">
       <h6 class="title"><b>タウンゼンド諸法(1767年)</b></h6>
       <hr>
@@ -23,48 +23,4 @@ export default {
 </script>
 
 <style scoped>
-.event .border{
-  position: fixed;
-  top: 50%;
-  left: 50%;
-  height: 50%;
-  transform: translate(-50%, -50%);
-  width: 1000px;
-  background: #ffffff;
-  z-index: 2001;
-  border-radius: 15px;
-  border: solid 5px burlywood;
-}
-.event .border .xButton{
-  position: fixed;
-  right: 20px;
-  top: -5px;
-  font-size: 40px;
-  cursor: pointer;
-  font-family: "Comic Sans MS";
-  font-weight: bold;
-}
-.event .border .title{
-  position: relative;
-  top: 20px;
-  font-size: 50px;
-}
-.event .border .context{
-  position: fixed;
-  top: auto;
-  font-size: 20px;
-  height: 600px;
-  overflow-y: auto;
-  font-weight: normal;
-  text-align: center;
-}
-.event .layer {
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100vh;
-  z-index: 2000;
-  opacity: 0.2;
-}
 </style>
