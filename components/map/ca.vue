56507f3af3363ee4ee4b19c9daea2e3f5134eae7
matoi0317 <matoi.shibanuma@gmail.com>
2020-11-07 16:19:19 +0900

    map2

--- components/map/ca.vue
+++ components/map/ca.vue
@@ -1,13 +1,13 @@
 <template>
   <div>
-    <div class="layer" v-if="true" @click="onClose">
+    <div class="layer" v-if="true" @click="onClose" :class="{isRemove: isRemove===true}">
       <div class="image-wrapper">
         <img class="note" src="~/assets/note.png">
         <div class="note-content">オスカー・グラント事件</div>
         <div class="note-content-sub">
           <br>サンフランシスコとベイエリアの各地をつなぐ公営高速鉄道システムの駅内で喧嘩をしていたと通報を受けた鉄道警察官らに電車から降ろされ、押さえつけられた状態で撃たれ死亡した事件。
         </div>
-        <div class="year">(2009年)</div>
+        <div class="year">(カリフォルニア州・2009年)</div>
       </div>
       <div class="map-wrapper">
         <img class="map" src="~/assets/note2.png">
@@ -22,16 +22,25 @@
 <script>
 export default {
   name: "ca.vue",
+  data(){
+    return{
+      isRemove: false
+    }
+  },
   methods:{
     onClose(){
-      this.$emit("onClose")
+      // this.$emit("onClose")
+      this.isRemove= true
+      setTimeout(() => {
+        this.$emit("onClose")
+      }, 500)
     }
   }
 }
 </script>
 
 <style scoped>
-@keyframes fadeInDown{
+@keyframes fadeIn{
   0% {
     opacity: 0;
   }
@@ -40,7 +49,7 @@ export default {
   }
 }
 
-@keyframes fadeOutUp{
+@keyframes fadeOut{
   0% {
     opacity: 1;
   }
@@ -57,7 +66,13 @@ export default {
   height:100%;
   background: rgba(0,0,0,0.9);
   z-index: 1000;
-  animation-name: fadeInDown;
+  animation-name: fadeIn;
+  animation-duration: 0.5s;
+  animation-timing-function: linear;
+}
+
+.layer.isRemove{
+  animation-name: fadeOut;
   animation-duration: 0.5s;
   animation-timing-function: linear;
 }
@@ -94,6 +109,7 @@ export default {
 .image-wrapper img{
   width: 100%;
   height:100%;
+  border-radius: 10px;
 }
 
 .map-wrapper img{
@@ -121,7 +137,7 @@ export default {
 .year{
   position: absolute;
   top: 23%;
-  left: 75%;
+  left: 38%;
   font-size: 30px;
   font-weight: 600;
   color: black;
@@ -133,7 +149,7 @@ export default {
   left: 6%;
   margin: 0 auto;
   width: 90%;
-  font-size: 30px;
+  font-size: 27px;
   color: black;
 }
 
--- components/map_eng/ca.vue
+++ components/map_eng/ca.vue
@@ -1,13 +1,20 @@
 <template>
   <div>
-    <div class="layer" @click="onClose"></div>
-    <div class="map-content">
-      <h6 class="title">California</h6>
-      <hr>
-      <p class="context"><b>Shooting of Oscar Grant</b>
-      <br>On January 1, 2009, Oscar Grant, a 22 year old man, was murdered at a subway station. He was detained along with approximately 20 passengers that were involved in a fight in the train. Although he was not involved in the fight, he was near the location the fight took place and was captured. While trying to confront the officer, he was backed against the wall and was handcuffed, and later murdered.
-      </p>
-      <br>
+    <div class="layer" v-if="true" @click="onClose" :class="{isRemove: isRemove===true}">
+      <div class="image-wrapper">
+        <img class="note" src="~/assets/note.png">
+        <div class="note-content">Shooting of Oscar Grant</div>
+        <div class="note-content-sub">
+          <br>On January 1, 2009, Oscar Grant, a 22 year old man, was murdered at a subway station. He was detained along with approximately 20 passengers that were involved in a fight in the train. Although he was not involved in the fight, he was near the location the fight took place and was captured. While trying to confront the officer, he was backed against the wall and was handcuffed, and later murdered. 
+        </div>
+        <div class="year">(California・2009)</div>
+      </div>
+      <div class="map-wrapper">
+        <img class="map" src="~/assets/note2.png">
+        <div class="map2-wrapper">
+          <img class="map2" src="~/assets/kari.png">
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -15,43 +22,142 @@
 <script>
 export default {
   name: "ca.vue",
+  data(){
+    return{
+      isRemove: false
+    }
+  },
   methods:{
     onClose(){
-      this.$emit("onClose")
+      // this.$emit("onClose")
+      this.isRemove= true
+      setTimeout(() => {
+        this.$emit("onClose")
+      }, 500)
     }
   }
 }
 </script>
 
 <style scoped>
+*{
+  font-family: Rockwell, "Courier New", Courier, Georgia,
+                 Times, "Times New Roman", serif;
+}
+
+@keyframes fadeIn{
+  0% {
+    opacity: 0;
+  }
+  100%{
+    opacity: 1;
+  }
+}
+
+@keyframes fadeOut{
+  0% {
+    opacity: 1;
+  }
+  100%{
+    opacity: 0;
+  }
+}
+
+
 .layer{
   position: fixed;
   top: 0;
   left: 0;
   width: 100%;
-  height: 100%;
-  background: rgba(0,0,0,0.5);
+  height:100%;
+  background: rgba(0,0,0,0.9);
   z-index: 1000;
-}
-.map-content{
-  position: fixed;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  width: 1000px;
-  background: #ffffff;
-  z-index: 1001;
-  border-radius: 15px;
-  border: solid 5px burlywood;
-}
-.context{
-  font-size: 30px;
+  animation-name: fadeIn;
+  animation-duration: 0.5s;
+  animation-timing-function: linear;
 }
 
-.title{
-  position: relative;
-  top: 20px;
-  font-size: 50px;
+.layer.isRemove{
+  animation-name: fadeOut;
+  animation-duration: 0.5s;
+  animation-timing-function: linear;
 }
+
+
+.image-wrapper{
+  position: relative;
+  top: 4%;
+  left: 10%;
+  width: 700px;
+  height: 550px;
+  z-index: 1;
+}
+
+.map-wrapper{
+  position: absolute;
+  top: 10%;
+  right: 8%;
+  width: 500px;
+  height: 500px;
+  z-index: 0;
+  transform: rotate(10deg);
+}
+
+.map2-wrapper{
+  position: absolute;
+  top: 10%;
+  right: 0%;
+  width: 500px;
+  height: 350px;
+  transform: rotate(-10deg);
+}
+
+.image-wrapper img{
+  width: 100%;
+  height:100%;
+  border-radius: 10px;
+}
+
+.map-wrapper img{
+  width: 100%;
+  height:100%;
+}
+
+.map2-wrapper img{
+  width: 100%;
+  height: 100%;
+}
+
+.note-content{
+  position: absolute;
+  top: 10%;
+  left: 10%;
+  width: 90%;
+  margin: 0 auto;
+  font-size: 45px;
+  font-weight: 700;
+  color: black;
+  /*background: linear-gradient(transparent 50%, #cd853f 50%);*/
+}
+
+.year{
+  position: absolute;
+  top: 23%;
+  left: 60%;
+  font-size: 25px;
+  font-weight: 600;
+  color: black;
+}
+
+.note-content-sub{
+  position: absolute;
+  top: 30%;
+  left: 6%;
+  margin: 0 auto;
+  width: 90%;
+  font-size: 22px;
+  color: black;
+}
+
 
 </style>
