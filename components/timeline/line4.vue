e7d87cb7153ce2cca8ed9ab30e5fc88be9208e25
KENKUN-1031 <kencoding1031@gmail.com>
2020-11-10 14:43:21 +0900

    timeline19

---
+++ components/timeline/line4.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <div class="layer" @click="onClose"></div>
+      <div class="event-modal">
+        <div class="event-title">第2章:独立戦争の開幕</div>
+        <!--イベント-->
+        <div class="event1" @click="onClick('event1')"></div>
+
+        <div class="linebar"></div>
+        <div class="year">
+          <h6>
+            <div class="timelineFirst">1854</div>
+          </h6>
+          <h6>
+            <div class="timelineLast">1865</div>
+          </h6>
+        </div>
+      </div>
+    <event1 v-if="locationName==='event1'" @onClose="locationName=null"></event1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "line4.vue"
+}
+</script>
+
+<style scoped>
+
+</style>
