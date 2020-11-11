a8a28485533cc7f53c5ca52a16c309d6fab8ff35
KENKUN-1031 <kencoding1031@gmail.com>
2020-11-11 16:02:32 +0900

    timeline21

---
+++ components/events/4rth/event1.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="event-sub-modal">
+    <div class="sublayer" @click="onClose"></div>
+    <div class="border">
+      <h6 class="title"><b>カンザス・ネブラスカ法(1854年)</b></h6>
+      <hr>
+      <p class="context">カンザス準州とネブラスカ準州を創設することを決定した法律であると同時に、両州を奴隷州とするのか自由州とするのかは住民たちの選択に任せるとした。その結果、カンザスに隣接するミズーリの街には北部の奴隷制反対派と南部の奴隷制賛成派が衝突し、それぞれ武装した異住民が押し寄せ、「血を流すカンザス」と呼ばれる一連の激しい衝突が起こる。
+      </p>
+      <h6 class="xButton" @click="onClose">x</h6>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "event1.vue",
+  methods:{
+    onClose(){
+      this.$emit("onClose")
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- components/timeline/line4.vue
+++ components/timeline/line4.vue
@@ -5,6 +5,7 @@
         <div class="event-title">第4章:南北戦争</div>
         <!--イベント-->
         <div class="event1" @click="onClick('event1')"></div>
+        <div class="event2" @click="onClick('event2')"></div>
 
         <div class="linebar"></div>
         <div class="year">
@@ -17,16 +18,52 @@
         </div>
       </div>
     <event1 v-if="locationName==='event1'" @onClose="locationName=null"></event1>
+    <event2 v-if="locationName==='event2'" @onClose="locationName=null"></event2>
   </div>
 </template>
 
 <script>
+import event1 from "~/components/events/4rth/event1"
+import event2 from "~/components/events/4rth/event2"
+
 export default {
   name: "line4.vue",
-  components:{}
+  components:{event1, event2, },
+  data(){
+    return{
+      locationName:null
+    }
+  },
+  methods:{
+    onClose(){
+      this.$emit("onClose")
+    },
+    onClick(locationName2){
+      this.locationName=locationName2
+    }
+  }
 }
 </script>
 
 <style scoped>
-
+.event-modal .event1{
+  position:relative;
+  top: calc(10% - 35px);
+  left: 65px;
+  width: 20px;
+  height: 20px;
+  background: black;
+  cursor: pointer;
+  border-radius: 7px;
+}
+.event-modal .event2{
+  position:relative;
+  top: calc(10% - 25px);
+  left:500px;
+  width: 20px;
+  height: 20px;
+  background: black;
+  cursor: pointer;
+  border-radius: 7px;
+}
 </style>
