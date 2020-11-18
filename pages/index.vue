a8e8097d3f5a1b57af7ebc7b8e6fc854e358a260
Kana Ohtake <kaoh2020@tisbrain.net>
2020-11-18 10:48:08 +0900

    home+survey

--- pages/index.vue
+++ pages/index.vue
@@ -7,69 +7,33 @@
           <source src="~/assets/douga.mp4" type="video/mp4" />
         </video>
       </div>
-      <p class="two">最近、ジョージフロイドが警官に窒息死された事件をきっかけに、黒人差別が注目される話題になりました。昔から世界的に問題となっている黒人差別。しかし、私たちは一体どこまで黒人差別の事を理解しているのでしょう？本当に私たちはこの問題を理解しているのか？実際、私たちはこの問題のことを調べた際、自分たちの黒人差別問題に対する理解がどれだけ少なかったかを知り、驚きました。
+     <br>
+     <h2 class="biggertitle"><mark>きっかけ</marK></h2>
+     
+     <br>
+     <h4 class="hometitle">サイトを作ろうとしたきっかけ</h4>
+      <p class="three">最近、ジョージフロイドが警官に窒息死された事件をきっかけに、黒人差別が注目される話題になりました。昔から世界的に問題となっている黒人差別。しかし、私たちは一体どこまで黒人差別の事を理解しているのでしょう？本当に私たちはこの問題を理解しているのか？実際、私たちはこの問題のことを調べた際、自分たちの黒人差別問題に対する理解がどれだけ少なかったかを知り、驚きました。
      </p>
-       <!-- <br>
-        この機会に、私たちはインターナショナルスクール（高1〜3）と日本の県立高校（高1〜2）で差別の意識に関するアンケートを取って見ました。国籍の違いはこのようになりました。
-
-        <div class="logograph">
-       <figure>
-        <img class="graphimage" src="~/assets/nationjap.png">
-        <img class="graphimage" src="~/assets/nationinter.png">
-        <figcaption>左：日本の県立高校　左：インターナショナルスクールの結果</figcaption>
-        </figure>
-
-        </div>
-    
-    <p class="two">後ほど、国籍の違いで黒人差別に対する意識の違いがどれだけ環境により違うことがわかりました。
-    <br>アンケート結果によると黒人差別に対する関心度を聞いてみると、インター生の大半は4〜5を選び、1を選んだ生徒は一人もいませんでした。しかし、日本人学校は1と答えた人は少ない物の、回答した人はいました。また、関心度の平均を計算すると、インター生は4.02に対し、日本人学校生徒は0.57低い3.45でした。このアンケートにより、日本人の高校生の差別、黒人差別に対する意識はインター生に比べ、低いということがわかりました。
-    
-    <div class="logograph">
-    <figure>
-        <img class="graphimage" src="~/assets/concernjap.png">
-        <img class="graphimage" src="~/assets/concerninter.png">
-        <figcaption>左：日本の県立高校　左：インターナショナルスクールの結果</figcaption>
-        </figure>
-    </div>
-
-     <p class="two">関心度の低さの理由の一つとして考えられるのは実際生徒が差別を受けたことがある、また見たことがあるかに影響されてると私たちは思いました。実際、差別を見たことがあると回答した生徒は日本の高校が28.5%に対し、インター生は73.6%でした。
-    
-    <div class="logograph">
-    <figure>
-        <img class="graphimage" src="~/assets/sawjap.png">
-        <img class="graphimage" src="~/assets/sawinter.png">
-        <figcaption>左：日本の県立高校　左：インターナショナルスクールの結果</figcaption>
-        </figure>
-    </div>
-
-    <p class="two">また、インター生の約58％は差別を受けたことがあると回答しました。その反面、日本の県立学校ではたったの3.3%しか受けたことがあると回答し増田。しかも、差別を受けたのは国内か国外かという質問の回答は、日本人の大半は国外と回答した物の、インター生の大半は国内（日本）で受けたという結果が出ました。この為、インター生は差別とは日本国内でもある問題と意識している反面、日本の高校生に取っては、差別とは余り縁がない問題と考えられことがわかります。
-    
-    <div class="logograph">
-    <figure>
-        <img class="graphimage" src="~/assets/experiencejap.png">
-        <img class="graphimage" src="~/assets/yesjap.png">
-        <figcaption>日本の県立高校の結果</figcaption>
-        </figure>
-    </div>
-    <div class="logograph">
-    <figure>
-        <img class="graphimage" src="~/assets/experienceinter.png">
-        <img class="graphimage" src="~/assets/yesinter.png">
-        <figcaption>インターナショナルスクールの結果</figcaption>
-        </figure>
-    </div>
-
-    <p class="two">
-        <br>しかし、差別とは縁がない、程遠い問題と意識されているが、インター生のアンケート結果が見せるように、日本でもある問題です。また、しかし、差別という物を身近でまず見つけるのは難しいことです。その為、今問題になっている米国の黒人差別の教材により、差別の意識をより深く理解して欲しいと思っています。問題は理解しなければ解決できません。曖昧な理解では通用しない場合が多いです。今話題になってるからこそ知る機会にする事を願います。
-        <br>
-        <b>*私たちがこのサイトで取り上げている黒人差別問題は繊細な課題です。バイアスは断じて禁じるように気遣っています。その為、意見や個人の考えなどは控えています。ご了承ください。</b></p>-->
-      <p class="two">
+      <br>
+     <h4 class="hometitle">友人の経験</h4>
+       <p class="three">
       あなた自身が差別を意識したことはありますか？また、差別の対象となったことはありますか？ 私たちの通う学校には日本人以外にも様々な国の生徒が通っています。なので、差別問題に対する反対意識が他の学校と比べ大きいと思います。 私たちの友人が見た目で差別を受けて悩んでいた事が黒人差別という話題を選んだ一つの理由です。 差別という話題を選んだきっかけの一つとして、友人が見た目で差別を経験し、悩んでいることでした。見た目だけで友達は他人の視線を感じ、不快な思いをしたと語っていました。 私たちが実際に家でゲームをプレイしていた際に“Black Lives Matter”と書かれた通知がきました。私はこの“Black Lives Matter”という言葉に興味を持ち、ネットで調べた結果、様々なゲームの大元の会社で従業員の一人が、お客さんの肌の色の応じて接客の態度変えていたという黒人差別にあたる行為が問題視されていたようなのです。 また、学校の授業でも最近米国で起きている様々な出来事や事件で、私たちの黒人差別への興味や意識が高まったので、この話題を深く調べたいと思いました。
       <br>
+      </p>
+       <br>
+      <h4 class="hometitle">何故アメリカを対象としたのか？</h4>
+      <p class="three">
       アメリカでジョージフロイドが警官に殺害された事件最近注目された事件の一つです。これをきっかけに、アメリカでの黒人差別という点に注目しました。また、他にも、ブリオナテイラーさんが殺された事件など、米国では他の国に比べ、黒人差別に関する事件が沢山あることに注目しました。これをきっかけにアメリカでの黒人差別の歴史や事件などに興味を持つことになりました。もちろん、米国以外にも黒人差別の歴史や事例はあります。それで、私たちが考えたのは、サイトを拝見してくれた人たちが米国での黒人差別をまず理解して興味を持つことで、他の国での黒人差別を調べるきっかけになれたらと考えました。この為、調べるのを一ヶ所に集中することを決断しました。
       <br>
+      </p>
+       <br>
+      <h4 class="hometitle">何故黒人差別にしたのか？</h4>
+      <p class="three">
       私たちの生活では気づかない内に様々な差別が起こっている場合が沢山あります。今話題されている黒人差別も差別の一つです。私たちが注目した点は話題になっているのに関わらず、身の回りの人たちは余り黒人差別のことを余り良く理解していない事です。また、黒人差別のことに関し、私たち自身も知らないことが沢山あります。私たちが疑問視していることが、他の人も疑問視しているかもしれない。この様な理由をきっかけに、黒人差別の教材を作成し、より多い人に理解して欲しいと思いました。この為、差別全体ではなく、黒人差別に注目しました。
       <br>
+      </p>
+
+      <p class="three">
       <b>*私たちがこのサイトで取り上げている黒人差別問題は繊細な課題です。バイアスは断じて禁じるように気遣っています。その為、意見や個人の考えなどは控えています。ご了承ください。</b>
       </p>
     </div> 
@@ -121,6 +85,14 @@ export default {
   font-size: 18px; 
 }
 
+.three{
+  font-family: "Noto Sans JP"; 
+  width: 53%; 
+  margin: 0 auto;
+  margin-top: 10px; 
+  font-size: 18px; 
+}
+
 .start{
   font-family: "Noto Sans JP"; 
   color: #000000;
@@ -133,24 +105,31 @@ export default {
   text-shadow: 0.5px 0.5px #f6d405;
   text-align: center; 
 }
-.drawing{
-  display: block;
-  margin-left: auto;
-  margin-right: auto;
-  width: 70%;
-  height: 575px; 
-  margin-top: 0px; 
+
+.biggertitle{
+  font-weight: bold;
+  font-size: 24px;
+  font-family: "Noto Sans JP"; 
+  margin: 0 auto;
+   width: 68%; 
 }
 
-.drawing1{
-  display: block;
-  margin-left: auto;
-  margin-right: auto;
-  width: 83%;
-  height: 500px; 
-  margin-top: 0px; 
-  margin-bottom: 30px;
+mark {
+  display: inline-block;
+  line-height: 0em;
+  padding-bottom: 0.5em;
+  background: #f6d405;
 }
+
+.hometitle{
+  font-weight: bold;
+  font-size: 24px;
+  font-family: "Noto Sans JP"; 
+  margin: 0 auto;
+   width: 53%; 
+
+}
+
 
 .graphimage{
     width: 35%; 
