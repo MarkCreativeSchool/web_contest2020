a06c43322ddd3a4bf98f930d3de379598d56f4e3
KENKUN-1031 <kencoding1031@gmail.com>
2020-11-21 15:11:42 +0900

    timeline33

---
+++ components/events_eng/1st/event0.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "event0.vue"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ components/events_eng/1st/event1.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "event1.vue"
+}
+</script>
+
+<style scoped>
+
+</style>
