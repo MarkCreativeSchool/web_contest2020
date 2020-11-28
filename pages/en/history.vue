e5e6c2ba068b7a7bf9b137ea755428b8426ecf83
KENKUN-1031 <kencoding1031@gmail.com>
2020-11-28 15:28:43 +0900

    timeline_fix6

--- pages/en/history.vue
+++ pages/en/history.vue
@@ -16,31 +16,31 @@
     <br>
       <div class="timeline">
         <div class="line">
-          <div class="linebar line1" @click="onClick('line1')"><p class="line_sub_name">Chapter 1:<br>The Discovery of the Continent / The Birth of 13 Colonies</p></div>
+          <div class="linebar line1_en" @click="onClick('line1')"><p class="line_sub_name_en">Chapter 1:<br>The Discovery of the Continent / The Birth of 13 Colonies</p></div>
         </div>
         <div class="line">
-          <div class="linebar line2" @click="onClick('line2')"><p class="line_sub_name">Chapter 2:<br>Start of the Revolutionary War</p></div>
+          <div class="linebar line2_en" @click="onClick('line2')"><p class="line_sub_name_en">Chapter 2:<br>Start of the Revolutionary War</p></div>
         </div>
         <div class="line">
-          <div class="linebar line3" @click="onClick('line3')"><p class="line_sub_name">Chapter 3:<br>History after the Revolutionary War</p></div>
+          <div class="linebar line3_en" @click="onClick('line3')"><p class="line_sub_name_en">Chapter 3:<br>History after the Revolutionary War</p></div>
         </div>
         <div class="line">
-          <div class="linebar line4" @click="onClick('line4')"><p class="line_sub_name">Chapter 4:<br>American Civil War</p></div>
+          <div class="linebar line4_en" @click="onClick('line4')"><p class="line_sub_name_en">Chapter 4:<br>American Civil War</p></div>
         </div>
         <div class="line">
-          <div class="linebar line5" @click="onClick('line5')"><p class="line_sub_name">Chapter 5:<br>World War I</p></div>
+          <div class="linebar line5_en" @click="onClick('line5')"><p class="line_sub_name_en">Chapter 5:<br>World War I</p></div>
         </div>
         <div class="line">
-          <div class="linebar line6" @click="onClick('line6')"><p class="line_sub_name">Chapter 6:<br>World War II</p></div>
+          <div class="linebar line6_en" @click="onClick('line6')"><p class="line_sub_name_en">Chapter 6:<br>World War II</p></div>
         </div>
         <div class="line">
-          <div class="linebar line7" @click="onClick('line7')"><p class="line_sub_name">Chapter 7:<br>Cold War</p></div>
+          <div class="linebar line7_en" @click="onClick('line7')"><p class="line_sub_name_en">Chapter 7:<br>Cold War</p></div>
         </div>
         <div class="line">
-          <div class="linebar line8" @click="onClick('line8')"><p class="line_sub_name">Chapter 8:<br>21st Century America</p></div>
+          <div class="linebar line8_en" @click="onClick('line8')"><p class="line_sub_name_en">Chapter 8:<br>21st Century America</p></div>
         </div>
         <div class="line">
-          <div class="linebar lastLine" @click="onClick('lastLine')"><p class="lastLine-name">The History of Racism</p></div>
+          <div class="linebar lastLine" @click="onClick('lastLine')"><p class="lastLine-name_en">The History of Racism</p></div>
         </div>
         <div class="year-main">
           <h6>1600</h6>
@@ -166,107 +166,59 @@ export default {
   cursor: pointer;
   border-radius: 7px;
 }
-.line1{
+.line1_en{
   position: absolute;
   width: 361px;
   left: 17px;
 }
-.line1 .line1-name{
-  position: relative;
-  top: 17px;
-  width: 397px;
-  font-size:12px;
-}
-.line2{
+.line2_en{
   position: absolute;
   left: 376px;
   width: 38px;
 }
-.line2 .line2-name{
-  position: relative;
-  top: 17px;
-  width: 241px;
-  font-size:12px;
-}
-.line3{
+.line3_en{
   position: absolute;
   left: 503px;
   width: 40px;
 }
-.line3 .line3-name{
-  position: relative;
-  top: 17px;
-  width: 269px;
-  font-size:12px;
-}
-.line4{
+.line4_en{
   position: absolute;
   left: 609px;
   width:30px;
 }
-.line4 .line4-name{
-  position: relative;
-  top: 17px;
-  width: 174px;
-  font-size:12px;
-}
-.line5{
+.line5_en{
   position: absolute;
   left: 725px;
   width: 20px;
 }
-.line5 .line5-name{
-  position: relative;
-  top: 17px;
-  width: 134px;
-  font-size:12px;
-}
-.line6{
+.line6_en{
   position: absolute;
   left: 765px;
   width: 20px;
 }
-.line6 .line6-name{
-  position: relative;
-  top: 17px;
-  width: 138px;
-  font-size:12px;
-}
-.line7{
+.line7_en{
   position: absolute;
   left: 787px;
   width: 35px;
 }
-.line7 .line7-name{
-  position: relative;
-  top: 17px;
-  width: 110px;
-  font-size:11px;
-}
-.line8{
+.line8_en{
   position:absolute;
   left: 890px;
   width: 80px;
-}
-.line8 .line8-name{
-  position: relative;
-  top: 17px;
-  width: 190px;
-  font-size:12px;
 }
 .lastLine{
   position: absolute;
   left: 20px;
   width: calc(100% - 40px);
 }
-.lastLine .lastLine-name{
+.lastLine .lastLine-name_en{
   position: relative;
   bottom: 22px;
   left: 5px;
   width: 156px;
   font-size:15px;
 }
-.line_sub_name{
+.line_sub_name_en{
   position: relative;
   top: 17px;
   padding-top: 2px;
--- pages/history.vue
+++ pages/history.vue
@@ -14,28 +14,28 @@
       <br>
       <div class="timeline">
         <div class="line">
-          <div class="linebar line1_en" @click="onClick('line1')"><p class="line_sub_name">第1章:<br>大陸の発見 / 13の植民地(13 colonies)の誕生</p></div>
+          <div class="linebar line1" @click="onClick('line1')"><p class="line_sub_name">第1章:<br>大陸の発見 / 13の植民地(13 colonies)の誕生</p></div>
         </div>
         <div class="line">
-          <div class="linebar line2_en" @click="onClick('line2')"><p class="line_sub_name">第2章:<br>独立戦争の開幕</p></div>
+          <div class="linebar line2" @click="onClick('line2')"><p class="line_sub_name">第2章:<br>独立戦争の開幕</p></div>
         </div>
         <div class="line">
-          <div class="linebar line3_en" @click="onClick('line3')"><p class="line_sub_name">第3章：<br>独立戦争後のアメリカの歩み</p></div>
+          <div class="linebar line3" @click="onClick('line3')"><p class="line_sub_name">第3章：<br>独立戦争後のアメリカの歩み</p></div>
         </div>
         <div class="line">
-          <div class="linebar line4_en" @click="onClick('line4')"><p class="line_sub_name">第4章:<br>南北戦争</p></div>
+          <div class="linebar line4" @click="onClick('line4')"><p class="line_sub_name">第4章:<br>南北戦争</p></div>
         </div>
         <div class="line">
-          <div class="linebar line5_en" @click="onClick('line5')"><p class="line_sub_name">第5章：<br>第一次世界大戦の幕開け</p></div>
+          <div class="linebar line5" @click="onClick('line5')"><p class="line_sub_name">第5章：<br>第一次世界大戦の幕開け</p></div>
         </div>
         <div class="line">
-          <div class="linebar line6_en" @click="onClick('line6')"><p class="line_sub_name">第6章：<br>第二次世界大戦</p></div>
+          <div class="linebar line6" @click="onClick('line6')"><p class="line_sub_name">第6章：<br>第二次世界大戦</p></div>
         </div>
         <div class="line">
-          <div class="linebar line7_en" @click="onClick('line7')"><p class="line_sub_name">第7章：<br>冷戦</p></div>
+          <div class="linebar line7" @click="onClick('line7')"><p class="line_sub_name">第7章：<br>冷戦</p></div>
         </div>
         <div class="line">
-          <div class="linebar line8_en" @click="onClick('line8')"><p class="line_sub_name">第8章：<br>21世期のアメリカ</p></div>
+          <div class="linebar line8" @click="onClick('line8')"><p class="line_sub_name">第8章：<br>21世期のアメリカ</p></div>
         </div>
         <div class="line">
           <div class="linebar lastLine" @click="onClick('lastLine')"><p class="lastLine-name">黒人差別の歴史</p></div>
@@ -181,42 +181,42 @@ export default {
   cursor: pointer;
   border-radius: 7px;
 }
-.timeline .line .line1_en{
+.timeline .line .line1{
   position: absolute;
   width: 361px;
   left: 17px;
 }
-.timeline .line .line2_en{
+.timeline .line .line2{
   position: absolute;
   left: 376px;
   width: 38px;
 }
-.line3_en{
+.line3{
   position: absolute;
   left: 503px;
   width: 40px;
 }
-.line4_en{
+.line4{
   position: absolute;
   left: 609px;
   width:30px;
 }
-.line5_en{
+.line5{
   position: absolute;
   left: 725px;
   width: 20px;
 }
-.line6_en{
+.line6{
   position: absolute;
   left: 765px;
   width: 20px;
 }
-.line7_en{
+.line7{
   position: absolute;
   left: 787px;
   width: 35px;
 }
-.line8_en{
+.line8{
   position:absolute;
   left: 890px;
   width: 80px;
