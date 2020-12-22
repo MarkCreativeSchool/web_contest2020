c961e80f1c5aa17424b08d67d93be9a90b1d5c00
matoi0317 <matoi.shibanuma@gmail.com>
2020-12-22 22:02:49 +0900

    chart

---
+++ components/Chart.vue
@@ -0,0 +1,32 @@
+<script>
+import { Line, Pie, Bar } from 'vue-chartjs'
+ 
+export default {
+  extends: Pie,
+  data() {
+    return {
+      chartData: {
+        labels: ['日本', '中国', '台湾・日本'],
+        datasets: [
+          {
+            label: 'Data One',
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.3)',
+                'rgba(54, 162, 235, 0.3)',
+                'rgba(75, 192, 192, 0.3)',
+                ],
+            data: [269, 2, 1],
+          },
+        ],
+      },
+      options: {
+        responsive: true,
+        maintainAspectRatio: false,
+      },
+    }
+  },
+  mounted() {
+    this.renderChart(this.chartData, this.options)
+  },
+}
+</script>
---
+++ pages/trial.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h2>チャートのテスト描画</h2>
+    <chart />
+  </div>
+</template>
+ 
+<script>
+import Chart from '~/components/Chart.vue'
+ 
+export default {
+  components: {
+    Chart,
+  },
+}
+</script>
